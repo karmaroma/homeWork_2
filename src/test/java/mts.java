2010047a5d03b596909e19c6144fc0cc2badc646
Roman_Golovin1 <romagolovin@gmail.com>
2021-12-01 00:35:56 +0500

    ppg add order

--- src/test/java/mts.java
+++ src/test/java/mts.java
@@ -2,6 +2,7 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selectors.byText;
@@ -15,16 +16,55 @@ public class mts {
     }
 
     @Test
-    void mtsTest1(){
-        String street = "Ленина";
-        String building = "101";
+    void PPG(){
+        String fillingLocation = "Facility-A";
+        String customer = "Rich Customer";
+        int customerNumber = 007;
+        String billingAddress = "avenue the5, ,NEW YORK, NY, US, 10011";
+        String name = "John Joe";
+        String addressLine1 = "66 Raton st.";
+        String addressLine2 = "77 Boca st.";
+        String city = "Boca Raton";
+        String country = "United States";
+        String contact = "John Joe";
+        String state = "Florida";
+        String zip = "20010";
+        String contactNum ="2771444";
 
-        open("https://e-burg.mts.ru/personal/dlya-doma/checkconnect/e-burg-city/3805553");
-        $("[data-suggest-id=suggestMtsTvCity]").setValue("Нижний Тагил").pressEnter();
-        $("[data-suggest-id=suggestStreet]").setValue(street).pressEnter();
-        $("[name=building]").setValue(building).pressEnter();
+        open("https://devppgsupplyorderingportal.azurewebsites.net/");
+        $(By.xpath("/html/body/app-root/div/mat-sidenav-container/mat-sidenav/div/mat-nav-list/mat-list-item[3]/div/a")).click();
+        $($x("/html/body/app-root/div/mat-sidenav-container/mat-sidenav-content/div[1]/app-customer-list/div[3]/div/div[1]/mat-card/mat-card-footer/div/div[2]/mat-icon[1]")).click();
+        $(By.id("mat-radio-2")).click();
+        $(By.className("mat-button-wrapper")).click();
+        $(By.xpath("/html/body/app-root/div/mat-sidenav-container/mat-sidenav-content/div[1]/app-order-add/div/form/mat-horizontal-stepper/div[2]/div[2]/div[1]/div[2]/mat-form-field/div/div[1]/div/input")).setValue(fillingLocation);
 
-        $(".home-internet-connect-form-message").shouldHave(Condition.text("К сожалению, по указанному адресу нет возможности подключить пакетную услугу МТС."));
+        $(By.id("mat-input-4")).setValue(customer);
+        $(By.id("mat-input-5")).setValue(String.valueOf(customerNumber));
+        $(By.id("mat-input-6")).setValue(billingAddress);
+
+        $(By.xpath("//*[@id=\"cdk-step-content-0-1\"]/div[1]/div[8]/button/span[1]")).click();
+        $(By.xpath("/html/body/div[2]/div[2]/div/mat-dialog-container/app-shipping-address-add/mat-dialog-content/form/div/div[1]/mat-form-field/div/div[1]/div/input")).setValue(name);
+        $(By.xpath("/html/body/div[2]/div[2]/div/mat-dialog-container/app-shipping-address-add/mat-dialog-content/form/div/div[2]/mat-form-field/div/div[1]/div/input")).setValue(addressLine1);
+        $(By.xpath("/html/body/div[2]/div[2]/div/mat-dialog-container/app-shipping-address-add/mat-dialog-content/form/div/div[3]/mat-form-field/div/div[1]/div/input")).setValue(addressLine2);
+        $(By.xpath("/html/body/div[2]/div[2]/div/mat-dialog-container/app-shipping-address-add/mat-dialog-content/form/div/div[4]/mat-form-field/div/div[1]/div/input")).setValue(city);
+        $(By.xpath("/html/body/div[2]/div[2]/div/mat-dialog-container/app-shipping-address-add/mat-dialog-content/form/div/div[5]/mat-form-field/div/div[1]/div/app-state-form-control/div/input")).setValue(state);
+        $(By.xpath("/html/body/div[2]/div[2]/div/mat-dialog-container/app-shipping-address-add/mat-dialog-content/form/div/div[6]/mat-form-field/div/div[1]/div/app-country-form-control/div/input")).setValue(country);
+        $(By.xpath("/html/body/div[2]/div[2]/div/mat-dialog-container/app-shipping-address-add/mat-dialog-content/form/div/div[7]/mat-form-field/div/div[1]/div/input")).setValue(zip);
+        $(By.xpath("/html/body/div[2]/div[2]/div/mat-dialog-container/app-shipping-address-add/mat-dialog-content/form/div/div[8]/mat-form-field/div/div[1]/div/input")).setValue(contact);
+        $(By.xpath("/html/body/div[2]/div[2]/div/mat-dialog-container/app-shipping-address-add/mat-dialog-content/form/div/div[9]/mat-form-field/div/div[1]/div/input")).setValue(contactNum);
+        $(By.xpath("/html/body/div[2]/div[2]/div/mat-dialog-container/app-shipping-address-add/mat-dialog-actions/button[1]")).click();
+
+
+
+
+
+
+
+
+
+        //$("[name=building]").setValue(building).pressEnter();
+
+        //$(".home-internet-connect-form-message").shouldHave(Condition.text("К сожалению, по указанному адресу нет возможности подключить пакетную услугу МТС."));
 
     }
 
